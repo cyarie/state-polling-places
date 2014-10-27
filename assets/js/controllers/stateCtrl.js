fc1c5d9c035d7f6b25af83609d65977df0264296
cyarie <cyarie@gmail.com>
2014-10-27 18:41:57 -0400

    No, it can't. Not in the way I was doing it.

--- assets/js/controllers/stateCtrl.js
+++ assets/js/controllers/stateCtrl.js
@@ -8,25 +8,4 @@ angular.module("stateCtrl", []).controller("stateController", function($scope, $
             console.log(states);
         });
 
-    $scope.test = function(d) {
-        if (active.node() === this) return reset();
-        active.classed("active", false);
-        active = d3.select(this).classed("active", true);
-
-        var bounds = path.bounds(d),
-              dx = bounds[1][0] - bounds[0][0],
-              dy = bounds[1][1] - bounds[0][1],
-              x = (bounds[0][0] + bounds[1][0]) / 2,
-              y = (bounds[0][1] + bounds[1][1]) / 2,
-              scale = .9 / Math.max(dx / width, dy / height),
-              translate = [width / 2 - scale * x, height / 2 - scale * y];
-
-        g.transition()
-              .duration(750)
-              .style("stroke-width", 1.5 / scale + "px")
-              .attr("transform", "translate(" + translate + ")scale(" + scale + ")");
-
-        console.log(d.properties.state);
-    }
-
 });
