36382fd3206cabccb3510227660812db6a78f0a8
cyarie <cyarie@gmail.com>
2014-10-27 15:21:57 -0400

    Testing typeahead

--- assets/js/controllers/stateCtrl.js
+++ assets/js/controllers/stateCtrl.js
@@ -1,1 +1,11 @@
-angular.module("stateCtrl", []).controller("stateController", function($scope, $http) {});
+angular.module("stateCtrl", []).controller("stateController", function($scope, $http, stateFactory) {
+    $scope.states = {};
+    $scope.selected = undefined;
+
+    stateFactory.getStates()
+        .then(function(states) {
+            $scope.states = states;
+            console.log(states);
+        })
+
+});
--- assets/js/services/stateService.js
+++ assets/js/services/stateService.js
@@ -1,1 +1,18 @@
-angular.module("stateService", []).factory("stateFactory", function($http, $q) {});
+angular.module("stateService", []).factory("stateFactory", function($http, $q) {
+    var service = {};
+    var apiUrl = "/api/states/";
+
+    service.getStates = function() {
+        var deferred = $q.defer();
+        $http.get(apiUrl)
+            .success(function(data) {
+                deferred.resolve(data);
+            })
+            .error(function() {
+                deferred.reject("THERE ARE NO MORE STATES!");
+            });
+        return deferred.promise;
+    };
+
+    return service;
+});
