eb656d4abcb5474b20dd98cc5224b40629cdafc9
cyarie <cyarie@gmail.com>
2014-10-28 15:32:46 -0400

    Pushing up a form and display, so I can more easily upload state data and check it out

--- assets/js/controllers/stateCtrl.js
+++ assets/js/controllers/stateCtrl.js
@@ -1,11 +1,24 @@
 angular.module("stateCtrl", []).controller("stateController", function($scope, $http, stateFactory) {
+    $scope.createState = {};
+    $scope.stateData = {};
     $scope.states = {};
     $scope.selected = undefined;
 
     stateFactory.getStates()
         .then(function(states) {
             $scope.states = states;
+            $scope.stateData = states;
             console.log(states);
         });
+
+    $scope.makeState = function() {
+        $http.post("/api/states/", $scope.createState)
+            .success(function(data) {
+                $scope.stateData = data;
+            })
+            .error(function(error) {
+                console.log(error)
+            })
+    }
 
 });
